762c23204b4fe804abcdb4f0ff94199e7775e3f5
Khan Fan <khan.fan@oriente.com>
2018-06-05 15:49:59 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <p id="element">{{ message }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello',
+    }
+  }
+}
+</script>
+
+<style>
+  body {
+    background: red;
+  }
+</style>
