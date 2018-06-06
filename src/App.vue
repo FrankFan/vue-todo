97bcb38ecce5055b36fcbb29a7535dfa3751b2bf
Khan Fan <khan.fan@oriente.com>
2018-06-06 11:38:00 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="test">
-    {{ message }}
+  <div id="app">
+    <div id="cover"></div>
+    <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from './todo/header.vue';
+
 export default {
+  components: {
+    Header,
+  },
   data() {
     return {
       message: 'hello world',
@@ -14,8 +20,22 @@ export default {
 }
 </script>
 
-<style>
-  #test {
-    background: transparent;
-  }
+<style lang="stylus" scoped>
+#app{
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+}
+#cover{
+  position absolute
+  left 0
+  top 0
+  right 0
+  bottom 0
+  background-color #999
+  opacity .9
+  z-index -1
+}
 </style>
